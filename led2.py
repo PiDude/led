5b6b0fee21e8684a75545fa260e698466cf3358b
PiDude <dxjones00@yahoo.com>
2015-09-23 14:18:12 +0000

    150923

--- led2.py
+++ led2.py
@@ -1,13 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
+# use GPIO numbering
 GPIO.setmode(GPIO.BCM)
 
+#setting up GPIO for LED segments
 segments = (4,17,27,22,18,23,24)
 
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, False)
+
+
+
+#setting up the GPIO to ingnite motor
+fire = 7
+GPIO.setup(fire, GPIO.OUT)
+GPIO.output(fire, False)
 
 
 num = {'':(0,0,0,0,0,0,0),
@@ -23,10 +32,15 @@ num = {'':(0,0,0,0,0,0,0),
     '9':(1,1,1,1,0,1,1)}
 
 
-array=[1,1,1,1,1,1,1],[1,1,1,1,1,1,0],[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],[0,1,1,0,0,1,1],[1,0,1,1,0,1,1],[1,0,1,1,1,1,1]
+# this is the backwards array starting at 5, ending at 0
+array=[1,0,1,1,0,1,1],[0,1,1,0,0,1,1],[1,1,1,1,0,0,1],[1,1,0,1,1,0,1],[0,1,1,0,0,0,0],[1,1,1,1,1,1,0]
 
+# this is the forward array
+#array=[1,1,1,1,1,1,1],[1,1,1,1,1,1,0],[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],[0,1,1,0,0,1,1],[1,0,1,1,0,1,1],[1,0,1,1,1,1,1]
 
-for x in range(6,0,-1):
+#countdown from 5 to 0.
+#when at 0 (x=5), then fire launch GPIO
+for x in range(0,6):
 
     GPIO.output(4, array[x][0])
     GPIO.output(17, array[x][1])
@@ -35,10 +49,21 @@ for x in range(6,0,-1):
     GPIO.output(18, array[x][4])
     GPIO.output(23, array[x][5])
     GPIO.output(24, array[x][6])
-    time.sleep(1.5)
 
+
+    if x == 5:
+        for y in range(0,1):
+            GPIO.output(fire, True)
+            time.sleep(0.8)
+
+    time.sleep(0.8)
+
+#turn all GPIO off
 for segment in segments:
     GPIO.output(segment, False)
+
+GPIO.output(fire, False)
+
 
 GPIO.cleanup()
 
